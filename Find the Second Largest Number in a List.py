f48415b4ec448a11864f77a0c64d198419617378
sejall <33982474+sejall@users.noreply.github.com>
2018-09-22 06:23:57 +0530

    Add files via upload

---
+++ Find the Second Largest Number in a List.py
@@ -0,0 +1,11 @@
+# Find the Second Largest Number in a List 
+
+
+
+a=[]
+n=int(input("Enter number of elements:"))
+for i in range(1,n+1):
+    b=int(input("Enter element:"))
+    a.append(b)
+a.sort()
+print("Second largest element is:",a[n-2])
